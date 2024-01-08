055b88fd008be853f8f0b7c895475478beebebe8
Dowhathouwilt <dev6573@gmail.com>
2024-01-08 17:38:13 +0300

    first commit

---
+++ app/src/main/java/com/example/codapizza/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.codapizza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import com.example.codapizza.model.Topping
+import com.example.codapizza.model.ToppingPlacement
+import com.example.codapizza.ui.ToppingCell
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToppingCell(
+                topping = Topping.Pepperoni,
+                placement = ToppingPlacement.Left,
+                onClickTopping = {}
+            )
+        }
+    }
+}
